7043291008823d674fc655e4f82168664595c669
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-03 10:28:59 +0530

    Problem 2 solved. Getting the last car make and car model of the inventory array using hgher order functions

---
+++ problem2.js
@@ -0,0 +1,15 @@
+// ==== Problem #2 ====
+// The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  Log the make and model into the console in the format of:
+// "Last car is a *car make goes here* *car model goes here*"
+function problem2(inventory){
+    try {
+        const lastCar = inventory.reduce((acc, curr) => {
+            return curr
+        });
+        return  `Last car is a ${lastCar.car_make} ${lastCar.car_model}`
+    } catch (error) {
+        console.log(`Invalid input. ${error}`);
+    };
+}
+
+module.exports.problem2 = problem2;
---
+++ test/testProblem2.js
@@ -0,0 +1,5 @@
+const data = require('./data.js')
+const fn = require('./../problem2.js')
+
+const result = fn.problem2(data.inventory);
+console.log(result);
